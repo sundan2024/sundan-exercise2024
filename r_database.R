90917076593541ae8548524004d8ecd29ec2afa9
sundan2024 <sundan@mail.ustc.edu.cn>
2024-04-14 15:31:49 +0800

    homework_05

--- r_database.R
+++ r_database.R
@@ -6,73 +6,140 @@
 #
 #Date:2024-04-10 
 #
-#Script Name:
+#Script Name:r_database.R
 #
-#Script Description:
+#Script Description:Rstudio与SQLite、PostgreSQL数据库的交互
 #
 #
 #SETUP ----------------------------------------------
 
-#安装一些需要的安装包，且在电脑本地下载了SQLite3及DB
+
+##01-基础配置
+# 安装reticulate包，用于在R中与Python交互
+install.packages('reticulate')
+
+# 安装Python环境
+reticulate::install_miniconda(force = TRUE)
+
+# 安装Python中的retriever包
+reticulate::py_install('retriever')
+
+# 安装rdataretriever包，用于在R中运行retriever
+install.packages('rdataretriever')
+
+# 检查并更新可用数据集
+rdataretriever::check_for_updates()
+
+# 检查rdataretriever包是否已成功安装
+"rdataretriever" %in% installed.packages()
+
+# 创建Python虚拟环境
 reticulate::virtualenv_create("r-reticulate")
-reticulate::py_install("keras")
-reticulate::py_install("tensorflow")
+
+# 安装所需的Python包
+reticulate::py_install(c("keras", "tensorflow"))
+
+# 安装R中需要其他的包
 install.packages("ade4")
 install.packages("RPostgreSQL")
 install.packages("RSQLite")
 install.packages("DBI")
+install.packages("RODBC")
+install.packages("odbc")
+install.packages("dplyr")
+install.packages("dbplyr")
+install.packages("RPostgres")
 
-#加载所需的安装包
+# 加载所需的包
 library(reticulate)
 library(rdataretriever)
 library(DBI)
 library(ade4)
 library(dplyr)
-library(keras)
+library(RODBC)
+library(odbc)
+library(dbplyr)
+library(RPostgres)
+library(RPostgreSQL)
 
-#加载python环境
+# 设置Python虚拟环境
 use_virtualenv("~/.virtualenvs/r-reticulate")
 py_config()
 
+
+
+##02-加载数据
 # 使用rdataretriever包下载Doubs数据集 但是发现rdataretriever包里没有，更新后仍然找不到
 help(package = "rdataretriever")
 dataset_names()
 rdataretriever::fetch("Doubs", quiet = TRUE, data_names = NULL)
 rdataretriever::datasets("doubs")
+
 rdataretriever::get_updates() 
 rdataretriever::fetch("Doubs", quiet = TRUE, data_names = NULL)
 
-# 使用ade4成功加载了doubs
+#后来成功使用ade4加载了doubs
 library(ade4)
 data("doubs")
 str(doubs)
+head(doubs)
 
-#下载"portal.sqlite",已使用DB打开查看
-download.file("https://ndownloader.figshare.com/files/11188550",
-              "portal.sqlite",
-              mode = "wb")
+
+
+##03-数据传输到SQLite数据库
+# 下载并查看"portal.sqlite"数据库
+download.file("https://ndownloader.figshare.com/files/11188550", "portal.sqlite", mode = "wb")
 getwd()
 
 #  查看数据集中列表类容，该数据集共有四个列表："env"、"fish"、"xy"、"species"。
-?doubs                           
+?doubs 
 
-# 创建连接到 SQLite 数据库
-con <- dbConnect(RSQLite::SQLite(), "C:/Users/Administrator/Documents/portal.sqlite")
+# 创建与SQLite数据库的连接
+con_1 <- dbConnect(RSQLite::SQLite(), "portal.sqlite")
 
-#查看现有数据库中的已存在数据
-dbListTables(con)
+# 查看数据库中的表
+dbListTables(con_1)
 
-#将doubs转换类型
+# 转换数据集并写入数据库
 combined_df <- purrr::map_dfr(doubs, as.data.frame)
+dbWriteTable(con_1, "Doubs", combined_df)
 
-#将转换后的doubs写入数据库，名为Doubs
-dbWriteTable(con, "Doubs", combined_df)
-
-#再次查看现有数据库中的已存在数据，已成功导入
-dbListTables(con)
+# 再次查看数据库中的表
+dbListTables(con_1)
 
 # 关闭数据库连接
-dbDisconnect(con)
+dbDisconnect(con_1)
 
+
+
+##04-数据传输到PostgreSQL数据库
+#现在开始尝试连接postgreSQL数据库，已经提前在电脑里下载了该程序并新建了一个Employees数据库
+con_2 <- dbCanConnect(RPostgres::Postgres(),
+                      dbname ="Employees",
+                      port =5432,
+                      user ="postgres",
+                      password="123456")
+con_2
+
+#上述结果返回TRUE，证明可以有效连接
+con_3 <-dbConnect(RPostgres::Postgres(),
+                  dbname ="Employees",
+                  port =5432,
+                  user = "postgres",
+                  password="123456")
+
+con_3
+
+# 查看PostgreSQL数据库中的表
+dbListTables(con_2)
+
+# 将数据传输到PostgreSQL数据库
+dbWriteTable(con_2, "Doubs", combined_df)
+
+# 再次查看PostgreSQL数据库中的表，已成功传输
+dbListTables(con_2)
+
+# 关闭数据库连接
+dbDisconnect(con_2)
 
 
